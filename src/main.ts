181ec251c2aa356ef15c9c2870ff7adf365a7bdc
jungsungwook <jswcyber@naver.com>
2023-05-18 14:34:21 +0900

    feat: dotevn

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import path from 'path';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
