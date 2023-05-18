eec55860e772531948dff60613c0cd624d7a8854
jungsungwook <jswcyber@naver.com>
2023-05-18 14:38:27 +0900

    feat: swaager api

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,20 @@ import { AppModule } from './app.module';
 import path from 'path';
 import * as dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  const options = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-documents', app, document);
   await app.listen(3000);
 }
 bootstrap();
