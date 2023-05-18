da405aebb8ab3fd4402aa3e6b87320157fda30c1
jungsungwook <jswcyber@naver.com>
2023-05-18 14:35:43 +0900

    feat: cookie parse

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import path from 'path';
 import * as dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
