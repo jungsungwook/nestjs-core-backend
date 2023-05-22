dafba7a39d8f7999e224d4193f7848b75ba6b7f4
jungsungwook <jswcyber@naver.com>
2023-05-22 09:57:21 +0900

    feat: middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,42 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { AuthTokenMiddleware } from './auth/authToken.middleware';
 
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 60 * 60 * 24,
+      max: 1000,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) as number,
+      username: process.env.DB_USER as string || 'abcd',
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      timezone: 'Z',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthTokenMiddleware)
+      .forRoutes({
+        path: '*',
+        method: RequestMethod.ALL,
+      });
+  }
+}
